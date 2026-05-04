3729d21ae0a55060806f34688b52cf9913a57900
ElZidane123 <elzidaneardyansyah265@gmail.com>
2026-05-04 11:32:29 +0700

    updt

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,16 @@
 import type { NextConfig } from 'next'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-      },
-      {
-        protocol: "https",
-        hostname: "arengaindonesia.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn1-production-images-kly.akamaized.net",
-      },
-      {
-        protocol: "https",
-        hostname: "asset.kompas.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.dapurkobe.co.id",
-      },
-      {
-        protocol: "https",
-        hostname: "richcreme.com",
-      },
-      // Tambahkan domain lain di sini kalau nambah gambar baru
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "arengaindonesia.com" },
+      { protocol: "https", hostname: "cdn1-production-images-kly.akamaized.net" },
+      { protocol: "https", hostname: "asset.kompas.com" },
+      { protocol: "https", hostname: "www.dapurkobe.co.id" },
+      { protocol: "https", hostname: "richcreme.com" },
     ],
   },
-};
-
-module.exports = nextConfig;
+}
 
 export default nextConfig
